36d3b4987ba2e4913745894d4c49403fc423f54f
Robert Chu <robert@wibidata.com>
2014-05-05 15:29:05 -0700

    Adds licenses etc.

---
+++ README.md
@@ -0,0 +1,8 @@
+Wibi ApplianceManager
+=====================
+
+This project represents an attempt at making YARN easier to use.
+
+Design Document: https://wiki.wibidata.com/display/ENG/WibiApplianceManager+Design+Document
+JIRA Project: https://jira.wibidata.com/browse/LOOM
+
--- src/test/java/com/wibidata/appliance/YarnClientTest.java
+++ src/test/java/com/wibidata/appliance/YarnClientTest.java
@@ -1,3 +1,22 @@
+/**
+ * (c) Copyright 2014 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wibidata.appliance;
 
 import java.util.UUID;
@@ -30,8 +49,6 @@ public class YarnClientTest {
         ResourceScheduler.class
     );
 
-    // This currently fails if multiple are instantiated.
-//    Preconditions.checkState(null != mCluster, "Cluster was already started!");
     mCluster = new MiniYARNCluster(
         String.format("test-yarn-cluster-%s", UUID.randomUUID().toString()),
         2,
